181d3647359e257869eb236f53697795e8c500bc
diegoandcontroll <lukasxdp2@gmail.com>
2022-07-03 04:17:34 -0300

    fix: adjustment transform button as anchor

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,22 @@
-import { ReactNode } from 'react';
+import { AnchorHTMLAttributes, ButtonHTMLAttributes, ReactNode } from 'react';
 import * as S from './styles';
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLButtonElement>;
 export type ButtonProps = {
-  children?: ReactNode;
   size?: 'small' | 'medium' | 'large';
   fullWidth?: boolean;
   icon?: ReactNode;
-};
+  as?: React.ElementType;
+} & ButtonTypes;
 const Button = ({
   children,
   size = 'medium',
   fullWidth = false,
   icon,
+  ...props
 }: ButtonProps) => (
-  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon}>
+  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
     {!!icon && icon}
     {!!children && <span>{children}</span>}
   </S.Wrapper>
