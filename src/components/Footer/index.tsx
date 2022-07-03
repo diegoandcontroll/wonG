04ff19a358286e697be7ed3b48a1e795a0ff3b1d
diegoandcontroll <lukasxdp2@gmail.com>
2022-07-03 03:42:49 -0300

    feat: create component Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,87 @@
+import * as S from './styles';
+import Logo from '../Logo';
+import Heading from 'components/Heading';
+import Link from 'next/link';
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <S.Wrapper>
+      <Logo color="black" />
+      <S.Content>
+        <S.Column>
+          <Heading color="black" size="small" lineColor="secondary" lineBottom>
+            Contact us
+          </Heading>
+          <a href="mailto:lukasxdp@gmail.com">lukasxdp@gmail.com</a>
+        </S.Column>
+
+        <S.Column aria-labelledby="social-media">
+          <Heading color="black" lineColor="secondary" lineBottom size="small">
+            Follow us
+          </Heading>
+
+          <nav id="social-media">
+            <a
+              href="https://www.instagram.com/won-games"
+              target="_blank"
+              rel="noopenner, noreferrer"
+            >
+              Instagram
+            </a>
+            <a
+              href="https://www.twitter.com/won-games"
+              target="_blank"
+              rel="noopenner, noreferrer"
+            >
+              Twitter
+            </a>
+            <a
+              href="https://www.youtube.com/won-games"
+              target="_blank"
+              rel="noopenner, noreferrer"
+            >
+              Youtube
+            </a>
+            <a
+              href="https://www.facebook.com/won-games"
+              target="_blank"
+              rel="noopenner, noreferrer"
+            >
+              Facebook
+            </a>
+          </nav>
+        </S.Column>
+
+        <S.Column aria-labelledby="resources">
+          <Heading color="black" lineColor="secondary" lineBottom size="small">
+            Links
+          </Heading>
+
+          <nav id="resources">
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+            <Link href="/games">
+              <a>Store</a>
+            </Link>
+            <Link href="/search">
+              <a>Buscar</a>
+            </Link>
+          </nav>
+        </S.Column>
+
+        <S.Column aria-label="contact">
+          <Heading color="black" lineColor="secondary" lineBottom size="small">
+            Location
+          </Heading>
+          <span>Lorem ipsum dolor sit.</span>
+          <span>Lorem Ipsum</span>
+          <span>Lorem, ipsum dolor.</span>
+        </S.Column>
+      </S.Content>
+      <S.Copyright>Won Games {currentYear} © All rights reserved.</S.Copyright>
+    </S.Wrapper>
+  );
+};
+
+export default Footer;
