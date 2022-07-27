ed93e65857e150b6967eaa5dbb2df11441d12f40
diegoandcontroll <lukasxdp2@gmail.com>
2022-07-27 09:31:51 -0300

    feat: create minimal button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,6 +6,7 @@ type ButtonTypes =
 export type ButtonProps = {
   size?: 'small' | 'medium' | 'large';
   fullWidth?: boolean;
+  minimal?: boolean;
   icon?: ReactNode;
   as?: React.ElementType;
 } & ButtonTypes;
@@ -14,9 +15,16 @@ const Button = ({
   size = 'medium',
   fullWidth = false,
   icon,
+  minimal = false,
   ...props
 }: ButtonProps) => (
-  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
+  <S.Wrapper
+    size={size}
+    fullWidth={fullWidth}
+    hasIcon={!!icon}
+    minimal={minimal}
+    {...props}
+  >
     {!!icon && icon}
     {!!children && <span>{children}</span>}
   </S.Wrapper>
