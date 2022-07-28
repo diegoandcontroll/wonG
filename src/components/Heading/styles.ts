daaa8e485b7d05090b5390d1c2a6d72c618cc13b
diegoandcontroll <lukasxdp2@gmail.com>
2022-07-28 12:08:56 -0300

    feat: create pages sign-in and sign-out

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -9,11 +9,16 @@ const wrapperModifiers = {
     }
   `,
   medium: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.huge};
+  `,
+
+  huge: (theme: DefaultTheme) => css`
     font-size: ${theme.font.sizes.xlarge};
     ${media.greaterThan('medium')`
       font-size: ${theme.font.sizes.xxlarge};
     `}
   `,
+
   lineLeft: (theme: DefaultTheme, lineColor: LineColors) => css`
     padding-left: ${theme.spacings.xxsmall};
     border-left: 0.7rem solid ${theme.colors[lineColor]};
---
+++ src/templates/Auth/index.tsx
@@ -0,0 +1,42 @@
+import Heading from 'components/Heading';
+import Logo from 'components/Logo';
+import Image from 'next/image';
+import * as S from './styles';
+export type AuthProps = {
+  title: string;
+  children: React.ReactNode;
+};
+const Auth = ({ children, title }: AuthProps) => (
+  <S.Wrapper>
+    <S.BannerBlock>
+      <Image
+        src="/img/auth-bg.jpg"
+        alt="Won Games Auth Page"
+        layout="fill"
+        objectFit="cover"
+      />
+      <S.BannerContent>
+        <Logo id="banner" />
+        <div>
+          <Heading>All your favorite games in one place</Heading>
+          <S.Subtitle>
+            <strong>WON</strong> is the best and most complete gaming platform
+          </S.Subtitle>
+        </div>
+
+        <S.Footer>Won Games 2022 © Todos os Direitos Reservados</S.Footer>
+      </S.BannerContent>
+    </S.BannerBlock>
+    <S.Content>
+      <S.ContentWrapper>
+        <Logo color="black" size="large" id="form" />
+        <Heading color="black" lineColor="secondary" lineLeft>
+          {title}
+        </Heading>
+        {children && children}
+      </S.ContentWrapper>
+    </S.Content>
+  </S.Wrapper>
+);
+
+export default Auth;
